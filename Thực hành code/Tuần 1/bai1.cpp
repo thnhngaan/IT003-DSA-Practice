08993d60dfc9a17c4d0f074f2d5ea81c3fca79aa
Thanhngaan <nganthanh368108@gmail.com>
2025-11-28 15:38:12 +0700

    First commit

---
+++ Thực hành code/Tuần 1/bai1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include<vector>
+#include <algorithm>
+#include <cmath> 
+using namespace std;
+
+struct Point{
+    float x, y, z; // Tạo 1 struct có cấu tạo x, y, z. Đây là những thành phần của điểm 3D
+};
+
+void input(vector <Point>& a, int n) { // Tạo 1 vector chứa các điểm 3D 
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].x >> a[i].y >> a[i].z; // Nhập lần lượt các giá trị x, y, z của vector 3D đó
+    }
+}
+
+float calculate(Point a) { // Tính khoảng cách của điểm 
+    return sqrt(a.x * a.x + a.y *a.y + a.z * a.z);
+}
+
+int main() {
+    vector<Point> a;
+    int n;
+    cin >> n;
+    a.resize(n); // cấp cho vector a có size n
+
+    float b, e;
+    input(a, n);
+    cin >> b >> e; // Nhập khoảng giá trị
+
+    bool flag = false; // đặt cờ
+    for (int i = 0; i < n; i++) {
+        if (calculate(a[i]) >= b && calculate(a[i]) <= e) {
+            cout << a[i].x << " " << a[i].y << " " << a[i].z << endl;
+            flag = true; // tìm ra được khoảng cách đúng điều kiện thì cập nhật flag = true
+        }
+    }
+    if (!flag) { // Không tìm ra thì cập nhật flag false
+        cout << "KHONG";
+    }
+    return 0;
+}
---
+++ Thực hành code/Tuần 1/bai2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include<vector>
+#include <algorithm>
+#include <cmath> 
+using namespace std;
+
+struct Point {
+	float x, y, z;
+};
+
+
+int main() {
+    int n;
+    int m;
+    cin >> n;
+
+    vector<Point> a(n); // khai báo vector a có khoảng n
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].x >> a[i].y >> a[i].z;
+    }
+
+    cin >> m;
+    vector<Point> b(m); // khai báo vector b có khoảng m
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i].x >> b[i].y >> b[i].z;
+    }
+
+    for (int i = 0; i < m; i++) { // duyệt từng điểm b[i] để tìm trong a
+        int pos = -1; // gọi pos = -1
+
+        for (int j = 0; j < n; j++) {   // tìm trong a
+            if (a[j].x == b[i].x &&
+                a[j].y == b[i].y &&
+                a[j].z == b[i].z) {
+                pos = j; // cập nhật giá trị pos
+                break; // chỉ lấy giá trị pos đầu tiên rồi chuyển sang giá trị khác       
+            }
+        }
+
+        if (pos == -1) {
+            cout << "KHONG\n";
+        }
+        else {
+            cout << pos << "\n";        
+        }
+    }
+
+    return 0;
+}
